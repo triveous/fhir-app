opensrp/fhircore

Add the new ecbis production flavor

android/quest/build.gradle.kts
@@ -168,6 +168,13 @@ android {
       dimension = "apps"
       applicationIdSuffix = ".ecbis"
       versionNameSuffix = "-ecbis"
+      manifestPlaceholders["appLabel"] = "MOH eCBIS Preview"
+    }
+    
+    create("ecbis_liberia") {
+      dimension = "apps"
+      applicationIdSuffix = ".ecbis_liberia"
+      versionNameSuffix = "-ecbis_liberia"
       manifestPlaceholders["appLabel"] = "MOH eCBIS"
     }
 

triveous/fhir-app

android/quest/build.gradle.kts
@@ -179,6 +179,13 @@ android {
       manifestPlaceholders["appLabel"] = "MOH eCBIS"
     }
 
+    create("ecbis_preview") {
+      dimension = "apps"
+      applicationIdSuffix = ".ecbis_preview"
+      versionNameSuffix = "-ecbis_preview"
+      manifestPlaceholders["appLabel"] = "MOH eCBIS Preview"
+    }
+
     create("g6pd") {
       dimension = "apps"
       applicationIdSuffix = ".g6pd"