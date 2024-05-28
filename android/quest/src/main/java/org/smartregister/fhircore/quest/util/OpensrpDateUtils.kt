d0464bf181d2da608ce3f4f9dc9d20ab4667e279
shivrajp1300 <shivaraj.p@triveous.com>
2024-05-28 20:38:05 +0530

    UI fixes

---
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/util/OpensrpDateUtils.kt
@@ -0,0 +1,15 @@
+package org.smartregister.fhircore.quest.util
+
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+object OpensrpDateUtils {
+
+    fun convertToDate(input: Date): String {
+        val outputFormat = SimpleDateFormat("dd MMMM yyyy", Locale.getDefault())
+
+        // Format the date to the desired output format
+        return outputFormat.format(input)
+    }
+}
