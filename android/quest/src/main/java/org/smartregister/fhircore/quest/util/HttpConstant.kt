ef891a4ee7306d8b7432525d8e869c15bffb900e
Jeetesh Surana <jeetesh.s@triveous.com>
2024-08-21 21:19:23 +0530

    DONE: - [X] AI recycled issue
    WIP:
    - [ ] Coming back in Draft it's not showing the AI data

---
+++ android/quest/src/main/java/org/smartregister/fhircore/quest/util/HttpConstant.kt
@@ -0,0 +1,8 @@
+package org.smartregister.fhircore.quest.util
+
+/**
+ * Created by Jeetesh Surana.
+ */
+
+const val SUSPICIOUS_NON_SUSPICIOUS_URL="https://midas.iisc.ac.in/CodeSystem/ai-result-value"
+const val CONFIDENCE_PERCENTAGE_URL="https://midas.iisc.ac.in/CodeSystem/ai-result-confidence"
