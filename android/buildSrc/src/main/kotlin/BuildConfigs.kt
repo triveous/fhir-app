c06c4c219c42bd41895154ce5a05eace65aacfc9
shivrajp1300 <shivaraj.p@triveous.com>
2024-08-22 01:03:11 +0530

    [BUGFIX] Fixed delete image issue

--- android/buildSrc/src/main/kotlin/BuildConfigs.kt
+++ android/buildSrc/src/main/kotlin/BuildConfigs.kt
@@ -2,8 +2,8 @@ object BuildConfigs {
     const val minSdk = 26
     const val compileSdk = 34
     const val targetSdk = 34
-    const val versionCode = 21
-    const val versionName = "1.0.0-beta8"
+    const val versionCode = 24
+    const val versionName = "1.0.0-beta11"
     const val applicationId = "org.smartregister.opensrp"
     const val jvmToolchain = 17
     const val kotlinCompilerExtensionVersion = "1.5.8"
