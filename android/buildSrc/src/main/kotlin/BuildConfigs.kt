d9bc01445365744e7a61baf4567984d29b62dab8
Jeetesh Surana <jeetesh.s@triveous.com>
2024-09-26 15:26:12 +0530

    DONE: - [X] Play store branch

--- android/buildSrc/src/main/kotlin/BuildConfigs.kt
+++ android/buildSrc/src/main/kotlin/BuildConfigs.kt
@@ -2,9 +2,9 @@ object BuildConfigs {
     const val minSdk = 26
     const val compileSdk = 34
     const val targetSdk = 34
-    const val versionCode = 27
-    const val versionName = "1.0.0-beta14"
-    const val applicationId = "org.smartregister.opensrp"
+    const val versionCode = 3
+    const val versionName = "AA_v1.0.1-$versionCode"
+    const val applicationId = "in.ac.iisc.arogyam"
     const val jvmToolchain = 17
     const val kotlinCompilerExtensionVersion = "1.5.8"
     const val jacocoVersion ="0.8.11"
