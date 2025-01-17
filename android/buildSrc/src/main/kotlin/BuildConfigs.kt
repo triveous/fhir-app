5ece734f8e6596f94d0177267b606321103d739e
shivrajp1300 <shivaraj.p@triveous.com>
2025-01-17 13:27:58 +0530

    AA_v1.2.7-12 release

--- android/buildSrc/src/main/kotlin/BuildConfigs.kt
+++ android/buildSrc/src/main/kotlin/BuildConfigs.kt
@@ -2,9 +2,9 @@ object BuildConfigs {
     const val minSdk = 26
     const val compileSdk = 34
     const val targetSdk = 34
-    const val versionCode = 28
-    const val versionName = "AA_v1.1.15-28"
-    const val applicationId = "org.smartregister.opensrp"
+    const val versionCode = 12
+    const val versionName = "AA_v1.2.7-12"
+    const val applicationId = "in.ac.iisc.arogyam"
     const val jvmToolchain = 17
     const val kotlinCompilerExtensionVersion = "1.5.8"
     const val jacocoVersion ="0.8.11"
