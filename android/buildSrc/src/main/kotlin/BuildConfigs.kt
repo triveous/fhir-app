e7ad2baf62b9ebc07b9ee038673347b508f304a9
shivrajp1300 <shivrajp130@gmail.com>
2026-09-22 14:59:46 +0530

    Bump version to 1.7.8; enforce English IME hint for numeric and string input fields

--- android/buildSrc/src/main/kotlin/BuildConfigs.kt
+++ android/buildSrc/src/main/kotlin/BuildConfigs.kt
@@ -2,8 +2,8 @@ object BuildConfigs {
     const val minSdk = 29
     const val compileSdk = 35
     const val targetSdk = 36
-    const val versionCode = 56
-    const val versionName = "AA_v1.7.7-56"
+    const val versionCode = 59
+    const val versionName = "AA_v1.7.8-59"
     const val applicationId = "in.ac.iisc.arogyam"
     const val jvmToolchain = 17
     const val kotlinCompilerExtensionVersion = "1.5.8"
